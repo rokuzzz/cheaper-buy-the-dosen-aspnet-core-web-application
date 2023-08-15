11ae4042afc90149d2b77843b53450358fc5c0f7
Roman Kuzero <rombeldan@gmail.com>
2023-08-15 11:11:52 +0300

    Build the ShoppingCart UI and add functionality to the buttons

--- DataAccess/Models/ShoppingCart.cs
+++ DataAccess/Models/ShoppingCart.cs
@@ -8,14 +8,20 @@ namespace DataAccess.Models
     {
         public int Id { get; set; }
         public int ProductId { get; set; }
+
         [ForeignKey("ProductId")]
         [ValidateNever]
         public Product? Product { get; set; }
+
         [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
         public string? ApplicationUserId { get; set; }
+
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser? ApplicationUser { get; set; }
+
+        [NotMapped]
+        public double CartPrice { get; set; }
     }
 }
